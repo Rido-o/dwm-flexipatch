b63431b7dd0a0b4c17aa70c6604fc6ac00a66f48
Rido <rido@airmail.cc>
2021-04-11 04:14:28 +0000

    renamed layoutmenu script

--- config.def.h
+++ config.def.h
@@ -306,7 +306,7 @@ static char *statuscolors[][ColCount] = {
 #endif // BAR_POWERLINE_STATUS_PATCH
 
 #if BAR_LAYOUTMENU_PATCH
-static const char *layoutmenu_cmd = "layoutmenu.sh";
+static const char *layoutmenu_cmd = "layoutmenu";
 #endif
 
 #if COOL_AUTOSTART_PATCH
